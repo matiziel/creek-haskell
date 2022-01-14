a54f0a26d7219e11d06ae440795107723581c11b
matiziel <matiziel10@gmail.com>
2022-01-14 04:10:35 +0100

    Set on board known cells

--- Board.hs
+++ Board.hs
@@ -2,11 +2,15 @@ module Board
   ( Board (..),
     BoardCell (..),
     BoardSize,
+    BoardCellIndex,
     generateBoard,
+    setBoardCells,
   )
 where
 
 type BoardSize = (Int, Int)
+
+type BoardCellIndex = (Int, Int)
 
 data BoardCell = Empty | Filled | Unknown deriving (Eq, Show)
 
@@ -16,3 +20,15 @@ generateBoard :: BoardSize -> Board
 generateBoard (height, width) = Board (height, width) cells
   where
     cells = [[Unknown | x <- [1 .. width]] | x <- [1 .. height]]
+
+boardCellValueAt :: Board -> BoardCellIndex -> BoardCell
+boardCellValueAt (Board _ cells) (xarg, yarg) = (cells !! xarg) !! yarg
+
+setBoardCell :: Board -> (BoardCellIndex, BoardCell) -> Board
+setBoardCell b@(Board (height, width) cells) ((xarg, yarg), value) =
+  Board
+    (height, width)
+    [[(if x == xarg && y == yarg then value else boardCellValueAt b (x, y)) | y <- [0 .. width - 1]] | x <- [0 .. height - 1]]
+
+setBoardCells :: Board -> [(BoardCellIndex, BoardCell)] -> Board
+setBoardCells = foldl setBoardCell
--- Main.hs
+++ Main.hs
@@ -15,6 +15,8 @@ main = do
   let rules = getRules creek
   print (solve board rules)
 
+-- print board
+
 getBoardSize :: Creek -> BoardSize
 getBoardSize (Creek size _) = size
 
@@ -22,10 +24,32 @@ getRules :: Creek -> Rules
 getRules (Creek _ rules) = rules
 
 solve :: Board -> Rules -> Board
-solve b@(Board (height, width) cells) rules = b
+solve b@(Board (height, width) cells) rules = filledCells
+  where
+    rulesWithZero = filter (filterRules 0) rules
+    emptyCells = fillCells b rulesWithZero Empty
+    rulesWithFour = filter (filterRules 4) rules
+    filledCells = fillCells emptyCells rulesWithFour Filled
 
-fillKnownEmptyCells :: Board -> Rules -> Board
-fillKnownEmptyCells b@(Board (height, width) cells) rules = b
+filterRules :: Int -> Rule -> Bool
+filterRules value ((a, b), c) = c == value
 
-filterRules :: Rule -> Int -> Bool
-filterRules r@((a, b), c) value = c == value
+fillCells :: Board -> Rules -> BoardCell -> Board
+fillCells board [] _ = board
+fillCells b (x : xs) value =
+  fillCells (applyRuleToBoard b x value) xs value
+
+applyRuleToBoard :: Board -> Rule -> BoardCell -> Board
+applyRuleToBoard b rule value = setBoardCells b [(index, value) | index <- getCellsForRuleApply b rule]
+
+getCellsForRuleApply :: Board -> Rule -> [(Int, Int)]
+getCellsForRuleApply (Board (height, width) cells) ((x, y), _)
+  | x == 0 && y == 0 = [(0, 0)]
+  | x == height && y == height = [(height - 1, width - 1)]
+  | x == 0 && y == width = [(0, y - 1)]
+  | x == height && y == 0 = [(height - 1, 0)]
+  | x == 0 = [(0, y - 1), (0, y)]
+  | x == height = [(height - 1, y - 1), (height - 1, y)]
+  | y == 0 = [(x - 1, 0), (x, 0)]
+  | y == width = [(x - 1, width - 1), (x, width - 1)]
+  | otherwise = [(xarg, yarg) | xarg <- [(x - 1) .. x], yarg <- [(y - 1) .. y]]
