6558c300d8389de8618fe298cd0ff2203de8c43f
matiziel <matiziel10@gmail.com>
2022-01-20 01:42:45 +0100

    Correct creek validation

--- Board.hs
+++ Board.hs
@@ -7,8 +7,11 @@ module Board
     boardCellValueAt,
     setBoardCells,
     replaceUknownCells,
+    validateCreek,
   )
 where
+
+import Data.Set (Set, empty, insert, notMember, size)
 
 type BoardSize = (Int, Int)
 
@@ -33,7 +36,8 @@ setBoardCell b@(Board (height, width) cells) ((xarg, yarg), value) =
     [[(if x == xarg && y == yarg then value else boardCellValueAt b (x, y)) | y <- [0 .. width - 1]] | x <- [0 .. height - 1]]
 
 setBoardCells :: Board -> [(BoardCellIndex, BoardCell)] -> Board
-setBoardCells = foldl setBoardCell
+setBoardCells b [] = b
+setBoardCells b (x : xs) = setBoardCells (setBoardCell b x) xs
 
 replaceUknownCells :: Board -> Board
 replaceUknownCells b@(Board (height, width) cells) =
@@ -48,8 +52,49 @@ replaceUknownCells b@(Board (height, width) cells) =
       | x <- [0 .. height - 1]
     ]
 
--- getCellsWithValue :: Board -> BoardCell -> [BoardCellIndex]
--- getCellsWithValue (Board (height, width) cells) value = [(1, 1)]
+validateCreek :: Board -> Bool
+validateCreek b@(Board (height, width) _) = size creek == length emptyOrUnknownCells
+  where
+    emptyOrUnknownCells = getEmptyOrUnknownCells b
+    -- firstEmpty = head emptyOrUnknownCells
+    creek = getCreekForValidation b [head emptyOrUnknownCells] empty
 
--- validateBoard :: Board -> Bool
--- validateBoard b@(Board (height, width) cells)
+getEmptyOrUnknownCells :: Board -> [BoardCellIndex]
+getEmptyOrUnknownCells b@(Board (height, width) _) =
+  filter (\(x, y) -> boardCellValueAt b (x, y) == Empty || boardCellValueAt b (x, y) == Unknown) cells
+  where
+    cells = generateBoardCells (height, width)
+
+generateBoardCells :: BoardSize -> [BoardCellIndex]
+generateBoardCells (height, width) = [(x, y) | x <- [0 .. height - 1], y <- [0 .. width - 1]]
+
+getCreekForValidation :: Board -> [BoardCellIndex] -> Set BoardCellIndex -> Set BoardCellIndex
+getCreekForValidation b [] set = set
+getCreekForValidation b (x : xs) set =
+  getCreekForValidation b (xs ++ neighboursNotVisited) newSet
+  where
+    newSet = insert x set
+    emptyNeighbours = getEmptyOrUnknownNeighbours b x
+    neighboursNotVisited = filter (\(x, y) -> notMember (x, y) set) emptyNeighbours
+
+getEmptyOrUnknownNeighbours :: Board -> BoardCellIndex -> [BoardCellIndex]
+getEmptyOrUnknownNeighbours b index =
+  filter (\(x, y) -> boardCellValueAt b (x, y) == Empty || boardCellValueAt b (x, y) == Unknown) neighbours
+  where
+    neighbours = getNeighbours b index
+
+getNeighbours :: Board -> BoardCellIndex -> [BoardCellIndex]
+getNeighbours b@(Board (height, width) cells) (x, y)
+  | x == 0 && y == 0 = [(x + 1, y), (x, y + 1)]
+  | x == (height - 1) && y == (width - 1) = [(x - 1, y), (x, y - 1)]
+  | x == 0 && y == (width - 1) = [(x + 1, y), (x, y - 1)]
+  | (x == height - 1) && y == 0 = [(x - 1, y), (x, y + 1)]
+  | y == 0 = [(x - 1, y), (x, y + 1), (x + 1, y)]
+  | x == 0 = [(x, y - 1), (x + 1, y), (x, y + 1)]
+  | y == (width - 1) = [(x - 1, y), (x, y - 1), (x + 1, y)]
+  | x == (height - 1) = [(x, y - 1), (x - 1, y), (x, y + 1)]
+  | otherwise = [(x, y - 1), (x - 1, y), (x, y + 1), (x + 1, y)]
+
+
+
+
